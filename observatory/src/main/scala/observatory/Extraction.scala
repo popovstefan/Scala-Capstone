9b4f44365fc4be09fe2b45ea5f08d8b28175afaf
Stefan Popov <popovstefan@live.com>
2021-01-21 16:33:35 +0100

    1st milestone

--- observatory/src/main/scala/observatory/Extraction.scala
+++ observatory/src/main/scala/observatory/Extraction.scala
@@ -2,10 +2,20 @@ package observatory
 
 import java.time.LocalDate
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+
+import scala.io.Source
+
 /**
   * 1st milestone: data extraction
   */
 object Extraction extends ExtractionInterface {
+  val appName = "Functional Programming in Scala - Capstone"
+  val spark: SparkSession = SparkSession.builder()
+    .appName(appName)
+    .master("local")
+    .getOrCreate()
 
   /**
     * @param year             Year number
@@ -14,7 +24,35 @@ object Extraction extends ExtractionInterface {
     * @return A sequence containing triplets (date, location, temperature)
     */
   def locateTemperatures(year: Year, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Temperature)] = {
-    ???
+    // Define column names
+    val stationColumnNames = List("STN_Identifier", "WBAN_Identifier", "Latitude", "Longitude")
+    val temperatureColumnNames = List("STN_Identifier", "WBAN_Identifier", "Month", "Day", "Temperature_F")
+    // Read the files
+    val stations: DataFrame = spark.read.format("csv")
+      .option("header", "false")
+      .load(stationsFile)
+      .toDF(stationColumnNames: _*)
+    val temperatures: DataFrame = spark.read.format("csv")
+      .option("header", "false")
+      .load(temperaturesFile)
+      .toDF(temperatureColumnNames: _*)
+    // Note on the "list: _*" syntax: https://stackoverflow.com/a/15034725/15052008
+    // Register DataFrames as TempViews
+    stations.createOrReplaceTempView("stations")
+    temperatures.createOrReplaceTempView("temperatures")
+    // Execute SQL query to transform data in a more convenient format
+    spark.sql(
+      """
+        |select t.Month, t.Day, s.Latitude, s.Longitude, ((t.Temperature_F - 32) / 1.8) as Temperature_C
+        |from stations as s inner join temperatures as t
+        |on s.STN_Identifier = t.STN_Identifier and s.WBAN_Identifier = t.WBAN_Identifier
+        |where s.Latitude is not null and s.Longitude is not null""".stripMargin)
+      .collect()
+      .map {
+        case Row(month: String, day: String, latitude: String, longitude: String, temperature: Double) =>
+          (LocalDate.of(year, month.toInt, day.toInt), Location(lat = latitude.toDouble, lon = longitude.toDouble), temperature)
+      }
+      .toSeq
   }
 
   /**
@@ -22,7 +60,17 @@ object Extraction extends ExtractionInterface {
     * @return A sequence containing, for each location, the average temperature over the year.
     */
   def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Temperature)]): Iterable[(Location, Temperature)] = {
-    ???
+
+    def sumTemperatures(groupedIterable: Iterable[(LocalDate, Location, Temperature)]): Double = {
+      groupedIterable.map(_._3).sum
+    }
+
+    val recordsRDD: RDD[(LocalDate, Location, Temperature)] = spark.sparkContext.parallelize(records.toSeq)
+    recordsRDD.groupBy(_._2)
+      .aggregateByKey(zeroValue = (0d, 0))(seqOp = (acc, groupedIterable) => (acc._1 + sumTemperatures(groupedIterable), acc._2 + groupedIterable.size), combOp = (acc1, acc2) => (acc1._1 + acc2._1, acc1._2 + acc2._2))
+      .mapValues(x => x._1 / x._2)
+      .collect()
+      .toSeq
   }
 
 }
--- observatory/src/test/scala/observatory/ExtractionTest.scala
+++ observatory/src/test/scala/observatory/ExtractionTest.scala
@@ -1,12 +1,44 @@
 package observatory
 
-import org.junit.Assert._
+import java.time.LocalDate
+
 import org.junit.Test
 
 trait ExtractionTest extends MilestoneSuite {
   private val milestoneTest = namedMilestoneTest("data extraction", 1) _
+  private val extractionObject = Extraction
 
   // Implement tests for the methods of the `Extraction` object
+  @Test def `test locateTemperatures with example from the instructions`(): Unit = {
+    val actual = extractionObject.locateTemperatures(year = 2015,
+      stationsFile = "/home/stefan/Projects/Coursera/Functional Programming in Scala Capstone/ScalaCapstone/observatory/src/main/resources/stations.csv",
+      temperaturesFile = "/home/stefan/Projects/Coursera/Functional Programming in Scala Capstone/ScalaCapstone/observatory/src/main/resources/2015.csv")
+    val expected = Seq(
+      (LocalDate.of(2015, 8, 11), Location(37.35, -78.433), 27.3),
+      (LocalDate.of(2015, 12, 6), Location(37.358, -78.438), 0.0),
+      (LocalDate.of(2015, 1, 29), Location(37.358, -78.438), 2.0)
+    )
+    expected.foreach { expectedElement =>
+      assert(actual.count(actualElement => actualElement._1.equals(expectedElement._1)
+        && actualElement._2.equals(expectedElement._2)
+        && actualElement._3.equals(expectedElement._3)) == 1, message = "Actual does not contain expected element: " + expectedElement)
+    }
+  }
 
+  @Test def `test locationYearlyAverageRecords with example from the instructions`(): Unit = {
+    val actual = extractionObject.locationYearlyAverageRecords(Seq(
+      (LocalDate.of(2015, 8, 11), Location(37.35, -78.433), 27.3),
+      (LocalDate.of(2015, 12, 6), Location(37.358, -78.438), 0.0),
+      (LocalDate.of(2015, 1, 29), Location(37.358, -78.438), 2.0)
+    ))
+    val expected = Seq(
+      (Location(37.35, -78.433), 27.3),
+      (Location(37.358, -78.438), 1.0)
+    )
+    expected.foreach { expectedElement =>
+      assert(actual.count(actualElement => actualElement._1.equals(expectedElement._1)
+        && actualElement._2.equals(expectedElement._2)) == 1, message = "Actual does not contain expected element: " + expectedElement)
+    }
+  }
 
 }
