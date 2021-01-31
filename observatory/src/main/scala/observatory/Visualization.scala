778710ee9e2798017a1f0324a97cabc9f0a15685
Stefan Popov <popovstefan@live.com>
2021-01-31 20:44:42 +0100

    2nd milestone completed

--- observatory/src/main/scala/observatory/Visualization.scala
+++ observatory/src/main/scala/observatory/Visualization.scala
@@ -1,14 +1,15 @@
 package observatory
 
-import com.sksamuel.scrimage.Image
+import com.sksamuel.scrimage.{Image, Pixel}
 
-import scala.annotation.tailrec
 import scala.math._
 
 /**
   * 2nd milestone: basic visualization
   */
 object Visualization extends VisualizationInterface {
+
+  val P = 3 // power parameter
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
@@ -17,25 +18,42 @@ object Visualization extends VisualizationInterface {
     */
   def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
 
-    def greatCircleDistance(loc1: Location, loc2: Location): Double = {
-
-      def isAntipodal(_loc1: Location, _loc2: Location): Boolean = {
-        if ((-1 * _loc2.lat).equals(_loc1.lat) && (_loc2.lon.equals(_loc1.lon + 180) || _loc2.lon.equals(_loc1.lon - 180)))
-          true
-        else
-          false
-      }
-
-      if (loc1.equals(loc2))
-        0
-      else if (isAntipodal(loc1, loc2) || isAntipodal(loc1, loc2))
-        Pi
-      else {
-        acos(sin(loc1.lat) * sin(loc2.lat) + cos(loc1.lat) * cos(loc2.lat) * cos(loc1.lon - loc2.lon))
-      }
+    def distance(l1: Location, l2: Location): Double = {
+      // Define earth radius variable
+      val earthRadiusKM = 6372.8
+      // Convert degrees to radians
+      val phi1 = math.toRadians(l1.lat)
+      val phi2 = math.toRadians(l2.lat)
+      val lambda1 = math.toRadians(l1.lon)
+      val lambda2 = math.toRadians(l2.lon)
+      // Apply formula
+      val greatCircleDistance = acos(sin(phi1) * sin(phi2) + cos(phi1) * cos(phi2) * cos(abs(lambda1 - lambda2)))
+      // Return result
+      earthRadiusKM * greatCircleDistance
     }
 
-    temperatures.map(it => 1 / pow(greatCircleDistance(it._1, location), 2) * it._2).sum / temperatures.size
+    def aggregateByWeightedInverseDistancing(distanceTemperaturePairs: Iterable[(Double, Temperature)], power: Int): Double = {
+      val (weightedSum, inverseWeightedSum) = distanceTemperaturePairs.aggregate((0.0, 0.0))(
+        { // sequential operation
+          case ((ws, iws), (distance, temp)) =>
+            val w = 1 / pow(distance, power)
+            (w * temp + ws, w + iws)
+        }, { // combination operation
+          case ((wsA, iwsA), (wsB, iwsB)) => (wsA + wsB, iwsA + iwsB)
+        }
+      )
+      weightedSum / inverseWeightedSum
+    }
+
+
+    val distanceTemperaturePairs: Iterable[(Double, Temperature)] = temperatures.map {
+      case (otherLocation, temperature) => (distance(otherLocation, location), temperature)
+    }
+    val nearbyTemperatures = distanceTemperaturePairs.filter(_._1 < 1)
+    if (nearbyTemperatures.isEmpty)
+      aggregateByWeightedInverseDistancing(distanceTemperaturePairs, power = P)
+    else
+      nearbyTemperatures.map(_._2).min
   }
 
   /**
@@ -44,39 +62,28 @@ object Visualization extends VisualizationInterface {
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Temperature, Color)], value: Temperature): Color = {
-    @tailrec
-    def findBelow(sortedIterable: Iterable[(Temperature, Color)]): (Temperature, Color) = {
-      if (sortedIterable.size < 2)
-        sortedIterable.head
-      else if (sortedIterable.tail.head._1 <= value)
-        sortedIterable.head
-      else
-        findBelow(sortedIterable.tail)
+    def inner(sortedPointsArray: Array[(Temperature, Color)]): Color = {
+      for (i <- 0 until sortedPointsArray.length - 1) {
+        (sortedPointsArray(i), sortedPointsArray(i + 1)) match {
+          case ((t1, Color(r1, g1, b1)), (t2, Color(r2, g2, b2))) =>
+            if (t1 > value)
+              return Color(r1, g1, b1)
+            else if (t2 > value) {
+              // Calculate the ratio between the distances of {t1} and {t2} with {value}
+              val ratio = (value - t1) / (t2 - t1)
+              // Calculate and round R, G, and B components
+              val r = math.round(r1 + (r2 - r1) * ratio)
+              val g = math.round(g1 + (g2 - g1) * ratio)
+              val b = math.round(b1 + (b2 - b1) * ratio)
+              return Color(r.toInt, g.toInt, b.toInt)
+            }
+        }
+      }
+      // If value is not within the list, return the maximum (i.e. last) color value
+      sortedPointsArray.last._2
     }
 
-    @tailrec
-    def findAbove(sortedIterable: Iterable[(Temperature, Color)]): (Temperature, Color) = {
-      if (sortedIterable.size < 2)
-        sortedIterable.head
-      else if (sortedIterable.head._1 >= value)
-        findAbove(sortedIterable.tail)
-      else
-        sortedIterable.tail.head
-    }
-
-    // Sort points
-    val sorted: Iterable[(Temperature, Color)] = points.toSeq.sortBy(_._1)(Ordering.Double.reverse)
-    // Find the first below and above the point we are trying to interpolate
-    val firstBelow: (Temperature, Color) = findBelow(sorted)
-    val firstAbove: (Temperature, Color) = findAbove(sorted)
-    // Calculate the ratio of the distances between the points above and below and our "value" point
-    val ratio = (firstAbove._1 - firstBelow._1) / (firstAbove._1 - value)
-    // Use this ratio to interpolate the R, G and B value
-    val red = firstBelow._2.red + (firstAbove._2.red - firstBelow._2.red) * ratio
-    val green = firstBelow._2.green + (firstAbove._2.green - firstBelow._2.green) * ratio
-    val blue = firstBelow._2.blue + (firstAbove._2.blue - firstBelow._2.blue) * ratio
-    // Return a Color object with the interpolated values
-    Color(red.toInt, green.toInt, blue.toInt)
+    inner(points.toList.sortBy(_._1)(Ordering.Double).toArray)
   }
 
   /**
@@ -85,7 +92,26 @@ object Visualization extends VisualizationInterface {
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)]): Image = {
-    ???
+
+    def pixelToLocation(x: Int, y: Int) = Location(lat = 90 - y, lon = x - 180)
+
+    // Initialize image properties
+    val alpha = 255
+    val imageWidth = 360
+    val imageHeight = 180
+    // Initialize pixel array
+    val imageBufferValues = new Array[Pixel](imageHeight * imageWidth)
+    // Iterate over each pixel and interpolate it's value
+    for (y <- 0 until imageHeight)
+      for (x <- 0 until imageWidth) {
+        // Predict temperature at the pixel
+        val temperatureAtPixel: Temperature = predictTemperature(temperatures, pixelToLocation(x, y))
+        // Interpolate the color based on the predicted temperature
+        val colorAtPixel: Color = interpolateColor(colors, temperatureAtPixel)
+        // Apply the interpolated color at the pixel
+        imageBufferValues(y * imageWidth + x) = Pixel.apply(colorAtPixel.red, colorAtPixel.green, colorAtPixel.blue, alpha)
+      }
+    Image(imageWidth, imageHeight, imageBufferValues)
   }
 
 }
